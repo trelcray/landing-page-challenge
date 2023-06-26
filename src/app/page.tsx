25ca70e4a7652e9e7ccf8fbeb0913d437e9b1681
trelcray <thaliszambarda@gmail.com>
2023-06-26 18:34:37 -0300

    feat: Add the marketing component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Banner } from "@/components/banner";
 import { Business } from "@/components/business";
+import { Marketing } from "@/components/marketing";
 
 export default function Home() {
   return (
-    <main className="flex flex-1 flex-col gap-6">
+    <main className="flex flex-1 flex-col gap-10">
       <Banner />
+      <Marketing />
       <Business />
     </main>
   );
