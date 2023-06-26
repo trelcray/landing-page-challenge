15827d19400f7ed487d63b38dc19186a0c93569c
trelcray <thaliszambarda@gmail.com>
2023-06-25 22:52:55 -0300

    build: Add the config for indicator

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,13 +7,25 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  container: {
+    center: true,
+    padding: "2rem",
+    screens: {
+      "2xl": "1400px",
+    },
+  },
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-plus-jakarta-sans)", ...fontFamily.sans],
       },
       colors: {
-        blue: { 200: "#f0f8ff", 400: "#2c83fb", 500: "#1f76f0" },
+        blue: {
+          200: "#f0f8ff",
+          300: "#e6f3ff",
+          400: "#2c83fb",
+          500: "#1f76f0",
+        },
         gray: { 500: "#6c8291", 800: "#1c3c50" },
       },
     },
