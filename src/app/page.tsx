214e4a3ba2991a309c6928573a207c6686db4068
trelcray <thaliszambarda@gmail.com>
2023-06-25 22:59:54 -0300

    feat: Add the business component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Banner } from "@/components/banner";
+import { Business } from "@/components/business";
 
 export default function Home() {
   return (
     <main className="flex flex-1 flex-col gap-6">
       <Banner />
+      <Business />
     </main>
   );
 }
