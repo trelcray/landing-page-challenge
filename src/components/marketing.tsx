eb8e07e4702bffd6add8009f8d5a312276f7e99c
trelcray <thaliszambarda@gmail.com>
2023-06-28 00:22:48 -0300

    refactor: Refactored the marketing

--- src/components/marketing.tsx
+++ src/components/marketing.tsx
@@ -3,11 +3,11 @@
 import { FC, useState } from "react";
 
 import { UseCreatePagination } from "@/hooks/use-create-pagination";
+import { UseWidth } from "@/hooks/use-width";
 import { videos } from "@/mocks/videos.json";
 import { cn } from "@/utils/cn";
 
 import { Button } from "./ui/button";
-import { CardVideo } from "./ui/card-video";
 import {
   Select,
   SelectContent,
@@ -15,43 +15,27 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./ui/select";
+import { VideoDialog } from "./video-dialog";
 
 export const Marketing: FC = ({}) => {
   const [isActive, setIsActive] = useState("Agências");
 
-  const { pagination, visibleResults, totalPages } = UseCreatePagination(
-    videos,
-    9
-  );
+  const { isMobile, isPortrait } = UseWidth(639, 1023);
 
-  const botoes = [];
+  const numberPages = isMobile ? 3 : isPortrait ? 6 : 9;
 
-  for (let i = 1; i <= totalPages; i++) {
-    const botao = (
-      <Button
-        key={i}
-        disabled={pagination.active === i}
-        onClick={() => pagination.setPage(i)}
-        variant="transparent"
-        size="xs"
-        className={cn(
-          "h-6 w-6 hover:border hover:border-blue-400",
-          "hover:bg-transparent hover:text-blue-400",
-          {
-            "border border-blue-500 font-bold text-blue-500":
-              pagination.active === i,
-          }
-        )}
-      >
-        <span className="text-sm">{i}</span>
-      </Button>
-    );
-    botoes.push(botao);
-  }
+  const { handleChangePage, visibleResults, pageLinks, currentPage } =
+    UseCreatePagination(videos, numberPages);
 
   return (
-    <section className="flex flex-1 flex-col items-center justify-center gap-5 px-2">
-      <div className="flex flex-wrap items-center justify-center gap-5 lg:w-[56rem] lg:justify-between">
+    <section
+      className="flex flex-1 flex-col items-center justify-center gap-5 
+      px-2"
+    >
+      <div
+        className="flex flex-wrap items-center justify-center gap-5 lg:w-[56rem]
+        lg:justify-between"
+      >
         <div className="flex flex-wrap items-center justify-center gap-2">
           {[
             "Agências",
@@ -80,35 +64,55 @@ export const Marketing: FC = ({}) => {
         </div>
         <div className="flex min-w-[17rem] items-center justify-end gap-2">
           <strong className="text-xs">Ordernar Por</strong>
-          <Select>
-            <SelectTrigger className="w-[180px] rounded-lg border-gray-800 px-2 text-xs">
-              <SelectValue placeholder="Selecione" />
+          <Select defaultValue="date">
+            <SelectTrigger
+              className="w-[180px] rounded-lg border-gray-800 
+              px-2 text-xs"
+            >
+              <SelectValue />
             </SelectTrigger>
             <SelectContent className="flex">
               <SelectItem value="date" className="text-sm">
                 Data de Publicação
               </SelectItem>
-              <SelectItem value="Nome" className="text-sm">
-                Nome
+              <SelectItem value="title" className="text-sm">
+                Título
               </SelectItem>
             </SelectContent>
           </Select>
         </div>
       </div>
 
-      <div className="grid grid-cols-1 place-content-start gap-4 border-y-2 py-10 sm:grid-cols-2 lg:grid-cols-3">
-        {visibleResults.map((item) => (
-          <CardVideo
-            title={item.titulo}
-            key={item.id}
-            urlImage={item.urlImage}
-          />
-        ))}
+      <div
+        className="grid min-h-[56rem] grid-cols-1 place-content-start gap-4 border-y-2 py-10
+        sm:grid-cols-2 lg:grid-cols-3"
+      >
+        <VideoDialog visibleResults={visibleResults} />
       </div>
 
       <div className="flex min-w-[12rem] gap-3">
         <strong>Página</strong>
-        <div className="flex gap-2">{botoes}</div>
+        <div className="flex gap-2">
+          {pageLinks.map((page) => (
+            <Button
+              key={page}
+              disabled={currentPage === page}
+              onClick={() => handleChangePage(page)}
+              variant="transparent"
+              size="xs"
+              className={cn(
+                "h-6 w-6 hover:border hover:border-blue-400",
+                "hover:bg-transparent hover:text-blue-400",
+                {
+                  "border border-blue-500 font-bold text-blue-500":
+                    currentPage === page,
+                }
+              )}
+            >
+              <span className="text-sm">{page}</span>
+            </Button>
+          ))}
+        </div>
       </div>
     </section>
   );
