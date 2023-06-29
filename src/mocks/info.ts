601752edc6ddc4b0bbc414db203c92df3cb0509e
trelcray <thaliszambarda@gmail.com>
2023-06-29 15:37:18 -0300

    feat: Created the info data

---
+++ src/mocks/info.ts
@@ -0,0 +1,92 @@
+import { FacebookIcon, Instagram, LinkedinIcon } from "lucide-react";
+
+export const Contact = {
+  title: "Siga a Leadster",
+  socials: [
+    {
+      name: "linkedin",
+      iconName: LinkedinIcon,
+      href: "https://br.linkedin.com/company/leadster-platform",
+    },
+    {
+      name: "facebook",
+      iconName: FacebookIcon,
+      href: "https://pt-br.facebook.com/leadsterplatform/",
+    },
+    {
+      name: "instagram",
+      iconName: Instagram,
+      href: "https://www.instagram.com/leadster.com.br/",
+    },
+  ],
+  contacts: [
+    {
+      name: "E-mail",
+      href: "contato@leadster.com.br",
+    },
+    {
+      name: "Telefone",
+      href: "(42) 98828-9851",
+    },
+  ],
+};
+
+export const Links = [
+  {
+    group: {
+      title: "Links Principais",
+      links: [
+        {
+          name: "Home",
+        },
+        {
+          name: "Ferramentas",
+        },
+        {
+          name: "Preços",
+        },
+        {
+          name: "Contato",
+        },
+      ],
+    },
+  },
+  {
+    group: {
+      title: "Cases",
+      links: [
+        {
+          name: "Geração de Leads B2B",
+        },
+        {
+          name: "Geração de Leads em Software",
+        },
+        {
+          name: "Geração de Leads em Imobiliária",
+        },
+        {
+          name: "Cases de Sucesso",
+        },
+      ],
+    },
+  },
+  {
+    group: {
+      title: "Materiais",
+      links: [
+        {
+          name: "Blog",
+        },
+        {
+          name: "Parceria com Agências",
+        },
+        {
+          name: "Guia Definitivo de Marketing",
+        },
+        {
+          name: "Materiais Gratuitos",
+        },
+      ],
+    },
+  },
+];
